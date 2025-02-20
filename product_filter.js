690256617ca4b9f841dcb9f6e12006b53c57ae1b
kavyakubireddi <kavya.ap.2001@gmail.com>
2025-02-20 11:08:45 +0530

    project added

---
+++ product_filter.js
@@ -0,0 +1,34 @@
+
+const filterButtons = document.querySelectorAll(".filter-buttons button");
+const productItems = document.querySelectorAll(".product-item");
+
+
+function filterProducts(category) {
+    for(let i=0;i<productItems.length;i++){
+        const item=productItems[i];
+        const itemCategory=item.getAttribute("data-category");
+
+        if(category==='all'  || itemCategory===category){
+            item.style.display="block";
+        }
+        else{
+            item.style.display="none";
+        }
+    }
+}
+
+for(let i=0;i<filterButtons.length;i++){
+    const button=filterButtons[i];
+    button.addEventListener("click",function(){
+        const filter=this.getAttribute("data-filter");
+        filterProducts(filter);
+           
+        for(let j=0;j<filterButtons.length;j++){
+            filterButtons[j].classList.remove("active");
+        }
+        this.classList.add("active");
+    });
+}
+
+filterProducts("all");
+filterButtons[0].classList.add("active");
